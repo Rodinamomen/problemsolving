beea66e96d7d3f9bfd78d3e6064d6322fcb526d0
Rodinamomen <Rodinamobark3@gmail.com>
2025-11-26 23:13:15 +0200

    brute force

---
+++ src/isAnagram.kt
@@ -0,0 +1,37 @@
+fun isAnagram(s: String, t: String): Boolean {
+    val stringMap : MutableMap<Char, Int> = mutableMapOf()
+    val secondStringMap : MutableMap<Char, Int> = mutableMapOf()
+    for(i in s){
+        if (stringMap.keys.contains(i)){
+            stringMap[i] = stringMap.getValue(i)+1
+        }
+        else{
+            stringMap[i] = 1
+        }
+    }
+    for(i in t){
+        if (secondStringMap.keys.contains(i)){
+            secondStringMap[i] = secondStringMap.getValue(i)+1
+        }
+        else{
+            secondStringMap[i] = 1
+        }
+    }
+    if(stringMap.size!= secondStringMap.size) {
+        return false
+    }else{
+        for(i in stringMap.keys){
+            if(secondStringMap.keys.contains(i)){
+                if(stringMap.getValue(i) != secondStringMap.get(i)){
+                    return false
+                }
+            }else{
+                return false
+            }
+        }
+    }
+    return true
+}
+fun main(){
+    print(isAnagram(s = "rat", t = "car"))
+}
