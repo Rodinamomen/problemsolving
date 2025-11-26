0261bf8c3c271403452edf602b09c4171e4d0c3c
Rodinamomen <Rodinamobark3@gmail.com>
2025-11-26 20:34:13 +0200

    using set

--- src/ContainsDuplicates.kt
+++ src/ContainsDuplicates.kt
@@ -1,13 +1,5 @@
 fun containsDuplicates(nums : Array<Int> ): Boolean{
-    var disArr = emptyArray<Int>()
-    for(i in nums.indices){
-        if(disArr.contains(nums[i])){
-            return true
-        }else{
-            disArr = disArr.plus(nums[i])
-        }
-    }
-    return false
+    return nums.size != nums.toSet().size
 }
 fun main(){
     print(containsDuplicates(arrayOf(1,4,2,3)))
