a6034d250f0e942ce6c7cf006068761bec09beef
Rodinamomen <Rodinamobark3@gmail.com>
2025-11-27 13:27:49 +0200

    doc

--- src/ValidParentheses.kt
+++ src/ValidParentheses.kt
@@ -1,5 +1,11 @@
 import java.util.Stack
-
+/**
+ * first we remove from our algorithm cases that will end up wrong like
+ * if the string length is odd
+ * or if the first string character is closing one
+ * then we check if the charter of type key we push it into the stack if not if the stack is empty then it should return false because that means we reach a point the stack does not have start char
+ * else we pop from rhw stack is the value of the map equals to char we continue
+ * we check that stack it is not empty at the end then return false else true*/
 fun isValid(s: String): Boolean {
     val map = mutableMapOf<Char, Char>('(' to ')', '[' to ']', '{' to '}')
     val stack = Stack<Char>()
